6d471a78131f80e74cb139a2e6dbd5ed00a57fa7
Paul Kramme <pjkramme@gmail.com>
2016-11-28 16:34:45 +0100

    Add getch and init

---
+++ getch.py
@@ -0,0 +1,24 @@
+def getch():
+    import termios, fcntl, sys, os
+    fd = sys.stdin.fileno()
+    flags_save = fcntl.fcntl(fd, fcntl.F_GETFL)
+    attrs_save = termios.tcgetattr(fd)
+    attrs = list(attrs_save)
+    attrs[0] &= ~(termios.IGNBRK | termios.BRKINT | termios.PARMRK
+                  | termios.ISTRIP | termios.INLCR | termios. IGNCR
+                  | termios.ICRNL | termios.IXON )
+    attrs[1] &= ~termios.OPOST
+    attrs[2] &= ~(termios.CSIZE | termios. PARENB)
+    attrs[2] |= termios.CS8
+    attrs[3] &= ~(termios.ECHONL | termios.ECHO | termios.ICANON
+                  | termios.ISIG | termios.IEXTEN)
+    termios.tcsetattr(fd, termios.TCSANOW, attrs)
+    fcntl.fcntl(fd, fcntl.F_SETFL, flags_save & ~os.O_NONBLOCK)
+    try:
+        ret = sys.stdin.read(1)
+    except KeyboardInterrupt:
+        ret = 0
+    finally:
+        termios.tcsetattr(fd, termios.TCSAFLUSH, attrs_save)
+        fcntl.fcntl(fd, fcntl.F_SETFL, flags_save)
+    return ret

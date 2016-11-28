4f84f776d5b22e0db5fd9ea516372e4bfe7a9b38
Paul Kramme <pjkramme@gmail.com>
2016-11-28 16:37:22 +0100

    Update getch.py

--- getch.py
+++ getch.py
@@ -4,14 +4,11 @@ def getch():
     flags_save = fcntl.fcntl(fd, fcntl.F_GETFL)
     attrs_save = termios.tcgetattr(fd)
     attrs = list(attrs_save)
-    attrs[0] &= ~(termios.IGNBRK | termios.BRKINT | termios.PARMRK
-                  | termios.ISTRIP | termios.INLCR | termios. IGNCR
-                  | termios.ICRNL | termios.IXON )
+    attrs[0] &= ~(termios.IGNBRK | termios.BRKINT | termios.PARMRK | termios.ISTRIP | termios.INLCR | termios. IGNCR | termios.ICRNL | termios.IXON )
     attrs[1] &= ~termios.OPOST
     attrs[2] &= ~(termios.CSIZE | termios. PARENB)
     attrs[2] |= termios.CS8
-    attrs[3] &= ~(termios.ECHONL | termios.ECHO | termios.ICANON
-                  | termios.ISIG | termios.IEXTEN)
+    attrs[3] &= ~(termios.ECHONL | termios.ECHO | termios.ICANON | termios.ISIG | termios.IEXTEN)
     termios.tcsetattr(fd, termios.TCSANOW, attrs)
     fcntl.fcntl(fd, fcntl.F_SETFL, flags_save & ~os.O_NONBLOCK)
     try:

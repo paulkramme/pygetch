e76fbfbd74b9a6df7986a7cbc594d8c30e9fc312
Paul Kramme <pjkramme@gmail.com>
2016-11-28 16:39:16 +0100

    Update getch.py

--- getch.py
+++ getch.py
@@ -4,7 +4,7 @@ def getch():
     flags_save = fcntl.fcntl(fd, fcntl.F_GETFL)
     attrs_save = termios.tcgetattr(fd)
     attrs = list(attrs_save)
-    attrs[0] &= ~(termios.IGNBRK | termios.BRKINT | termios.PARMRK | termios.ISTRIP | termios.INLCR | termios. IGNCR | termios.ICRNL | termios.IXON )
+    attrs[0] &= ~(termios.IGNBRK | termios.BRKINT | termios.PARMRK | termios.ISTRIP | termios.INLCR | termios.IGNCR | termios.ICRNL | termios.IXON )
     attrs[1] &= ~termios.OPOST
     attrs[2] &= ~(termios.CSIZE | termios. PARENB)
     attrs[2] |= termios.CS8
